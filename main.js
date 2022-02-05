427d6695e6466b9404632cf40bc48a09589b839c
Nick <92036651+dpotomahin@users.noreply.github.com>
2022-02-05 10:13:52 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+// While 
+
+// let a = 0;
+//     num = +prompt('Сколько чисел?')
+// while ( a <= num){
+//     console.log(a);
+//     a++;
+// }
+
+// Do...While
+//  let a = 0;
+//  do{
+//     a++;
+//     console.log(a);
+//  } while(a < 0)
+
+// let a = 1;
+// while ( a <= 10 ){
+//     y = a**2;
+//     console.log(`Число ${a}, квадрат которого равен ${y}`);
+//     a++;
+// }
+
+
+
+// Количество цыфр в числе
+// let message = +prompt("Введите некое число") 
+//     a = 0; 
+// while (message >= 1) { 
+//     message/=10; 
+//     a++; 
+// } 
+// alert("Количество цыфр в числе: " + a);
+
+//k!= 1*2*3*...*k
+// let k = +prompt('Введите конечное значение');
+//     s = 1;
+//     a = 1
+// while( s < k){
+//     console.log(s)
+//     s*=a
+//     a++
+// }
+// Вычислить сумму ряда
+// let n = +prompt('Введите n');
+//     a = 1/2;
+//     s = 0;
+//     i = 1;
+// while( i<=n ){
+//     s = s+a;
+//     i = i+1;
+//     a = a/2;
+//     console.log(s)
+// }    
+// alert('Результат ' + s)
